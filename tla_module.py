c03722dc9241a14713faf44e76886029a4d38ee0
David Yue <davidyue5819@gmail.com>
2026-03-22 19:29:05 -0600

    add files for tlagen

---
+++ tla_module.py
@@ -0,0 +1,215 @@
+type MappingIndex = str | int
+type MappingValue = Expr
+
+
+class Expr:
+    def __init__(self) -> None:
+        pass
+
+
+class Literal(Expr):
+    def __init__(self, value: int | str) -> None:
+        super().__init__()
+        self.value = value
+
+    def __str__(self) -> str:
+
+        if isinstance(self.value,  str):
+            return f'"{self.value}"'
+        else:
+            return str(self.value)
+
+
+class Variable(Expr):
+    def __init__(self, name: str) -> None:
+        super().__init__()
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+    def next(self):
+        return Next(self)
+
+
+class Next(Expr):
+    def __init__(self, v: Variable) -> None:
+        super().__init__()
+        self.v = v
+
+    def __str__(self):
+        return str(self.v) + "'"
+
+
+class Index(Expr):
+    def __init__(self, value: Expr, index: Expr) -> None:
+        super().__init__()
+        self.value = value
+        self.index = index
+
+    def __str__(self) -> str:
+        return str(self.value) + f"[{str(self.index)}]"
+
+
+class Constant(Expr):
+    def __init__(self, name: str) -> None:
+        super().__init__()
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+
+class Mapping(Expr):
+    def __init__(
+        self, indicies: list[MappingIndex], values: list[MappingValue]
+    ) -> None:
+        super().__init__()
+        self.indicies = indicies
+        self.values = values
+        assert len(indicies) == len(values)
+
+    def __str__(self):
+        return "[" + ", ".join([f"{i} |-> {v}" for i, v in zip(self.indicies, self.values)]) + "]"
+
+class IfThenElse(Expr):
+
+    def __init__(self, condition: Expr, if_body: Expr, else_body: Expr) -> None:
+        super().__init__()
+        self.condition = condition
+        self.if_body = if_body
+        self.else_body = else_body
+
+    def __str__(self) -> str:
+        return f"IF {str(self.condition)} THEN {str(self.if_body)} ELSE {str(self.else_body)}"
+
+
+class BinOp(Expr):
+    def __init__(self, op: str, lhs: Expr, rhs: Expr) -> None:
+        super().__init__()
+        self.lhs = lhs
+        self.rhs = rhs
+        self.op = op
+
+    def __str__(self):
+        return "(" + str(self.lhs) + " " + self.op + " " + str(self.rhs) + ")"
+
+
+class Add(BinOp):
+    def __init__(self, lhs: Expr, rhs: Expr) -> None:
+        super().__init__("+", lhs, rhs)
+
+
+class Sub(BinOp):
+    def __init__(self, lhs: Expr, rhs: Expr) -> None:
+        super().__init__("-", lhs, rhs)
+
+
+class And(BinOp):
+    def __init__(self, lhs: Expr, rhs: Expr) -> None:
+        super().__init__("/\\", lhs, rhs)
+
+
+class Or(BinOp):
+    def __init__(self, lhs: Expr, rhs: Expr) -> None:
+        super().__init__("\\/", lhs, rhs)
+
+
+class Equal(BinOp):
+    def __init__(self, lhs: Variable | Next, rhs: Expr) -> None:
+        super().__init__("=", lhs, rhs)
+
+
+class Definition(Expr):
+    def __init__(self, name: str, value: Expr) -> None:
+        super().__init__()
+        self.name = name
+        self.value = value
+
+    def __str__(self):
+        return self.name
+
+    def toDefString(self):
+        return str(self) + " == " + str(self.value)
+
+
+class MappingUpdate(Expr):
+    def __init__(
+        self, mapping: Variable, updates: list[tuple[Literal, Expr]]
+    ) -> None:
+        super().__init__()
+        self.mapping = mapping
+        self.updates = updates
+
+    def __str__(self):
+        return f"[{str(self.mapping)} EXCEPT " + ", ".join([f"![{str(i)}] = {str(v)}" for i, v in self.updates]) + "]"
+
+class TLAModule:
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.variables: list[Variable] = []
+        self.constants: list[Constant] = []
+        self.definitions: list[Definition] = []
+        self.initialState: Definition | None = None
+        self.nextState: Definition | None = None
+
+    def createVariable(self, name: str) -> Variable:
+        v = Variable(name)
+        self.variables.append(v)
+        return v
+
+    def createConstant(self, name: str):
+        c = Constant(name)
+        self.constants.append(c)
+        return c
+
+    def createDefinition(self, name: str, expr: Expr):
+        d = Definition(name, expr)
+        self.definitions.append(d)
+        return d
+
+    def setInitialState(self, expr: Expr):
+        self.initialState = Definition("Init", expr)
+
+    def setNextState(self, expr: Expr):
+        self.nextState = Definition("Next", expr)
+
+    def __str__(self):
+
+        lines = []
+
+        assert self.initialState is not None
+        assert self.nextState is not None
+
+        moduleHeader = "-" * 10 + " MODULE " + self.name + " " + 10 * "-"
+
+        lines.append(moduleHeader)
+
+        if len(self.variables) > 0:
+            lines.append(f"VARIABLES {', '.join([str(v) for v in self.variables])}")
+
+        if len(self.constants) > 0:
+            lines.append(f"CONSTANTS {', '.join([str(v) for v in self.constants])}")
+
+        lines.append(self.initialState.toDefString())
+
+        for d in self.definitions:
+            lines.append(d.toDefString())
+
+        lines.append(self.nextState.toDefString())
+
+        lines.append("=" * len(moduleHeader))
+
+        return "\n".join(lines)
+
+
+if __name__ == "__main__":
+    module = TLAModule("Hello")
+
+    a = module.createVariable("a")
+    b = module.createVariable("b")
+    c = module.createVariable("c")
+
+    module.setInitialState(And(a, b))
+    module.setNextState(Or(a, b))
+    print(module)
---
+++ tla_thread.py
@@ -0,0 +1,206 @@
+from tla_module import (
+    TLAModule,
+    Mapping,
+    MappingUpdate,
+    MappingValue,
+    MappingIndex,
+    Definition,
+    IfThenElse,
+    And,
+    Or,
+    Add,
+    Equal,
+    Literal,
+    Expr,
+    Index,
+)
+from functools import reduce
+
+
+class TLAProcess(TLAModule):
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
+        self.mem = self.createConstant("mem")
+        self.thread_initial_states = []
+        self.thread_step_states = []
+        self.threads: list[TLAThread] = []
+
+    def createThread(
+        self,
+        name: str,
+        registers: list[MappingIndex],
+        initialRegisterValues: list[MappingValue],
+    ) -> "TLAThread":
+        thread = TLAThread(self, name, registers, initialRegisterValues)
+
+        self.threads.append(thread)
+
+        return thread
+
+    def addThreadInitialState(self, state: Expr):
+        self.thread_initial_states.append(state)
+
+    def addThreadStepState(self, state: Definition):
+        self.thread_step_states.append(state)
+
+    def __str__(self):
+
+        for t in self.threads:
+            t._createStepState()
+
+        self.setInitialState(
+            reduce(
+                lambda accum, x: And(accum, x),
+                self.thread_initial_states[1:],
+                self.thread_initial_states[0],
+            )
+        )
+
+        self.setNextState(
+            reduce(
+                lambda accum, x: Or(accum, x),
+                self.thread_step_states[1:],
+                self.thread_step_states[0],
+            )
+        )
+
+        return super().__str__()
+
+
+class TLAThread:
+    def __init__(
+        self,
+        process: TLAProcess,
+        thread_name: str,
+        registers: list[MappingIndex],
+        initialRegisterValues: list[MappingValue],
+    ) -> None:
+
+        self.process = process
+        self.pc_states = []
+        self.registers = registers
+        self.thread_name = thread_name
+        self.pc = self.process.createVariable(f"pc_{thread_name}")
+        self.reg_mapping = Mapping(registers, initialRegisterValues)
+        self.regs = self.process.createVariable(f"regs_{thread_name}")
+        start_state = self._createNewState("start")
+
+        self.process.addThreadInitialState(
+            And(
+                Equal(self.pc, Literal(start_state)), Equal(self.regs, self.reg_mapping)
+            )
+        )
+
+        self.thread_states: list[Definition] = []
+        self.current_state = start_state
+
+    def _uniqueName(self, suffix: str) -> str:
+
+        return f"{self.thread_name}_step_{str(len(self.pc_states))}_{suffix}"
+
+    def _currentState(self) -> str:
+        return self.current_state
+
+    def _getCurrentStep(self) -> int:
+
+        return len(self.pc_states)
+
+    def _createNewState(self, name: str = "") -> str:
+        state_name = self._uniqueName("state") if len(name) == 0 else name
+        self.pc_states.append(state_name)
+        self.setState(state_name)
+        return state_name
+
+    def _pcTransition(self, current: str, next: str):
+        return And(
+            Equal(self.pc, Literal(current)), Equal(self.pc.next(), Literal(next))
+        )
+
+    def _createStepState(self):
+        stepDef = self.process.createDefinition(
+            self._uniqueName("step"),
+            reduce(
+                lambda accum, x: Or(accum, x),
+                self.thread_states[1:],
+                self.thread_states[0],
+            ),
+        )
+        self.process.addThreadStepState(stepDef)
+
+    def _goto(self, toState: str) -> Expr:
+
+        return And(
+            Equal(self.pc, Literal(self._currentState())),
+            Equal(self.pc.next(), Literal(toState)),
+        )
+
+    def setState(self, newState):
+        self.current_state = newState
+
+    def getRegister(self, name: str):
+
+        return Index(self.regs, Literal(name))
+
+    def appendInstruction(self, instruction_name: str, expr: Expr) -> str:
+
+        currentState = self._currentState()
+
+        pc_transition = self._pcTransition(currentState, self._createNewState())
+
+        definition = self.process.createDefinition(
+            self._uniqueName(instruction_name),
+            And(pc_transition, expr),
+        )
+        self.thread_states.append(definition)
+
+        return currentState
+
+    def appendRegisterInstruction(
+        self,
+        instruction_name: str,
+        destination_register: str,
+        source: Expr,
+    ) -> str:
+        return self.appendInstruction(
+            instruction_name,
+            Equal(
+                self.regs.next(),
+                MappingUpdate(self.regs, [(Literal(destination_register), source)]),
+            ),
+        )
+
+    def appendBranchInstruction(
+        self, condition: Expr, true_state: str, false_state: str
+    ):
+        # assume no tla constants
+
+        definition = self.process.createDefinition(
+            f"branch_{true_state}_{false_state}",
+            IfThenElse(
+                condition,
+                self._goto(true_state),
+                self._goto(false_state),
+            ),
+        )
+
+        self.thread_states.append(definition)
+
+
+if __name__ == "__main__":
+    tlaproc = TLAProcess("hello")
+    tlathread = tlaproc.createThread(
+        "t1", [f"r{c}" for c in range(0, 10)], [Literal(0)] * 10
+    )
+    s1 = tlathread.appendRegisterInstruction(
+        "add_r0_r1", "r0", Add(tlathread.getRegister("r0"), tlathread.getRegister("r1"))
+    )
+    s2 = tlathread.appendRegisterInstruction(
+        "add_r0_r1", "r0", Add(tlathread.getRegister("r0"), tlathread.getRegister("r1"))
+    )
+
+    tlathread.appendBranchInstruction(
+        Add(tlathread.getRegister("r0"), tlathread.getRegister("r1")), s1, s2
+    )
+
+    print(tlaproc)
+    # tlaasm.appendInstruction("add_r0_r1", ))
